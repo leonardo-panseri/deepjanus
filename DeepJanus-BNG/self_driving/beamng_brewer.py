2fb6155b7d0977cf188ec8fb8287f405f44ad5fe
Leonardo Panseri <leonardo.panseri@gmail.com>
2023-11-03 18:16:15 +0100

    Refactor beamng_brewer.py: replace BeamNGCamera with Camera, move initialization of all car sensors here, convert angle of vehicle to quaternion, set framerate limit for simulator as it is now needed for deterministic mode

--- DeepJanus-BNG/self_driving/beamng_brewer.py
+++ DeepJanus-BNG/self_driving/beamng_brewer.py
@@ -1,5 +1,6 @@
 from beamngpy import BeamNGpy, Scenario, Vehicle
-from beamngpy.sensors import Camera
+from beamngpy.sensors import Camera, GForces, Electrics, Damage, Timer
+from beamngpy.misc.quat import angle_to_quat
 from core.folder_storage import SeedStorage
 from self_driving.beamng_config import BeamNGConfig
 from self_driving.beamng_member import BeamNGMember
@@ -8,35 +9,24 @@ from self_driving.decal_road import DecalRoad
 from self_driving.road_points import List4DTuple, RoadPoints
 from self_driving.simulation_data import SimulationParams
 from self_driving.beamng_pose import BeamNGPose
-
-
-class BeamNGCamera:
-    def __init__(self, beamng: BeamNGpy, name: str, camera: Camera = None):
-        self.name = name
-        self.pose: BeamNGPose = BeamNGPose()
-        self.camera = camera
-        if not self.camera:
-            self.camera = Camera((0, 0, 0), (0, 0, 0), 120, (1280, 1280), colour=True, depth=True, annotation=True)
-        self.beamng = beamng
-
-    def get_rgb_image(self):
-        self.camera.pos = self.pose.pos
-        self.camera.direction = self.pose.rot
-        cam = self.beamng.render_cameras()
-        img = cam[self.name]['colour'].convert('RGB')
-        return img
+from udacity_integration.beamng_car_cameras import BeamNGCarCameras
 
 
 class BeamNGBrewer:
     def __init__(self, road_nodes: List4DTuple = None):
-        self.beamng = BeamNGpy('localhost', 64256)
+        self.beamng = BeamNGpy('localhost', 12345)
         self.vehicle: Vehicle = None
-        self.camera: BeamNGCamera = None
+        self.use_camera = False
+        self.camera: Camera = None
+        self.car_cameras: BeamNGCarCameras = None
         if road_nodes:
             self.setup_road_nodes(road_nodes)
         steps = 5
         self.params = SimulationParams(beamng_steps=steps, delay_msec=int(steps * 0.05 * 1000))
         self.vehicle_start_pose = BeamNGPose()
+
+        self.beamng.open()
+        self.beamng.logger.setLevel(20)
 
     def setup_road_nodes(self, road_nodes):
         self.road_nodes = road_nodes
@@ -44,29 +34,41 @@ class BeamNGBrewer:
         self.road_points = RoadPoints().add_middle_nodes(road_nodes)
 
     def setup_vehicle(self) -> Vehicle:
-        assert self.vehicle is None
         self.vehicle = Vehicle('ego_vehicle', model='etk800', licence='TIG', color='Red')
+        self.vehicle.attach_sensor('gforces', GForces())
+        self.vehicle.attach_sensor('electrics', Electrics())
+        self.vehicle.attach_sensor('damage', Damage())
+        self.vehicle.attach_sensor('timer', Timer())
         return self.vehicle
 
-    def setup_scenario_camera(self, resolution=(1280, 1280), fov=120) -> BeamNGCamera:
-        assert self.camera is None
-        self.camera = BeamNGCamera(self.beamng, 'brewer_camera')
+    def setup_car_cameras(self) -> BeamNGCarCameras:
+        self.car_cameras = BeamNGCarCameras(self.beamng, self.vehicle)
+        return self.car_cameras
+
+    def setup_scenario_camera(self, resolution=(1280, 1280), fov=120) -> Camera:
+        assert self.use_camera is False
+        self.use_camera = True
         return self.camera
 
     def bring_up(self):
         self.scenario = Scenario('tig', 'tigscenario')
         if self.vehicle:
-            self.scenario.add_vehicle(self.vehicle, pos=self.vehicle_start_pose.pos, rot=self.vehicle_start_pose.rot)
-
-        if self.camera:
-            self.scenario.add_camera(self.camera.camera, self.camera.name)
+            self.scenario.add_vehicle(self.vehicle, pos=self.vehicle_start_pose.pos,
+                                 rot_quat=angle_to_quat(self.vehicle_start_pose.rot))
 
         self.scenario.make(self.beamng)
-        if not self.beamng.server:
-            self.beamng.open()
-        self.beamng.pause()
-        self.beamng.set_deterministic()
+
         self.beamng.load_scenario(self.scenario)
+        self.beamng.set_deterministic(60)
+        self.beamng.pause()
+
+        if self.use_camera:
+            self.camera = Camera('brewer_camera', self.beamng, is_static=True, pos=(0, 0, 0), dir=(0, 0, 0), field_of_view_y=120,
+                                 resolution=(1280, 1280))
+
+        if self.car_cameras:
+            self.car_cameras.setup_cameras()
+
         self.beamng.start_scenario()
 
     def __del__(self):
@@ -82,16 +84,16 @@ if __name__ == '__main__':
     brewer = BeamNGBrewer()
     vehicle = brewer.setup_vehicle()
     camera = brewer.setup_scenario_camera()
-    while True:
-        seed_storage = SeedStorage('basic5')
-        for i in range(1, 11):
-            member = BeamNGMember.from_dict(seed_storage.load_json_by_index(i))
-            brewer.setup_road_nodes(member.sample_nodes)
-            brewer.vehicle_start_pose = brewer.road_points.vehicle_start_pose()
-            brewer.bring_up()
-            print('bring up ok')
-            brewer.beamng.resume()
-            print('resumed')
-            input('waiting keypress...')
-            print('key received')
-            brewer.beamng.stop_scenario()
+
+    seed_storage = SeedStorage('basic5')
+    member = BeamNGMember.from_dict(seed_storage.load_json_by_index(1))
+    brewer.setup_road_nodes(member.sample_nodes)
+    brewer.vehicle_start_pose = brewer.road_points.vehicle_start_pose()
+
+    brewer.bring_up()
+    print('bring up ok')
+    brewer.beamng.resume()
+    print('resumed')
+    input('waiting keypress...')
+    print('key received')
+    brewer.beamng.stop_scenario()
