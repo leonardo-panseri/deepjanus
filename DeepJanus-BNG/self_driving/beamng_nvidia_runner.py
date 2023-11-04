2130ae3de0b675cd3d9459eb8cd370751431cbdb
Leonardo Panseri <leonardo.panseri@gmail.com>
2023-11-04 11:01:35 +0100

    Refactor beamng_nvidia_runner.py: modify calls to brewer and vehicle state reader, make simulation run for 1 step at start, modify method to access camera

--- DeepJanus-BNG/self_driving/beamng_nvidia_runner.py
+++ DeepJanus-BNG/self_driving/beamng_nvidia_runner.py
@@ -1,8 +1,9 @@
-import logging as log
 import os
 import time
 import traceback
-from typing import List, Tuple
+from typing import List, Tuple, Optional
+
+from PIL import Image
 
 from core.folder_storage import SeedStorage
 from core.folders import folders
@@ -18,7 +19,6 @@ from self_driving.simulation_data import SimulationDataRecord, SimulationData
 from self_driving.simulation_data_collector import SimulationDataCollector
 from self_driving.utils import get_node_coords, points_distance
 from self_driving.vehicle_state_reader import VehicleStateReader
-from udacity_integration.beamng_car_cameras import BeamNGCarCameras
 
 log = get_logger(__file__)
 
@@ -28,7 +28,7 @@ FloatDTuple = Tuple[float, float, float, float]
 class BeamNGNvidiaOob(BeamNGEvaluator):
     def __init__(self, config: BeamNGConfig):
         self.config = config
-        self.brewer: BeamNGBrewer = None
+        self.brewer: Optional[BeamNGBrewer] = None
         self.model_file = str(folders.trained_models_colab.joinpath(config.keras_model_file))
         if not os.path.exists(self.model_file):
             raise Exception(f'File {self.model_file} does not exist!')
@@ -47,7 +47,7 @@ class BeamNGNvidiaOob(BeamNGEvaluator):
                     raise Exception('Exhausted attempts')
                 if attempt > 1:
                     log.info(f'RETRYING TO run simulation {attempt}')
-                    self._close()
+                    # self._close()
                 else:
                     log.info(f'{member} BeamNG evaluation start')
                 if attempt > 2:
@@ -62,35 +62,41 @@ class BeamNGNvidiaOob(BeamNGEvaluator):
     def _run_simulation(self, nodes) -> SimulationData:
         if not self.brewer:
             self.brewer = BeamNGBrewer()
-            self.vehicle = self.brewer.setup_vehicle()
             self.camera = self.brewer.setup_scenario_camera()
 
         brewer = self.brewer
         brewer.setup_road_nodes(nodes)
         beamng = brewer.beamng
         waypoint_goal = BeamNGWaypoint('waypoint_goal', get_node_coords(nodes[-1]))
-        maps.install_map_if_needed()
+        # TODO Fix this (path is relative to where this function is called)
+        # maps.install_map_if_needed()
         maps.beamng_map.generated().write_items(brewer.decal_road.to_json() + '\n' + waypoint_goal.to_json())
 
-        cameras = BeamNGCarCameras()
-        vehicle_state_reader = VehicleStateReader(self.vehicle, beamng, additional_sensors=cameras.cameras_array)
+        self.vehicle = self.brewer.setup_vehicle()
+        brewer.setup_car_cameras()
         brewer.vehicle_start_pose = brewer.road_points.vehicle_start_pose()
+
+        self.vehicle_state_reader = VehicleStateReader(self.vehicle)
 
         steps = brewer.params.beamng_steps
         simulation_id = time.strftime('%Y-%m-%d--%H-%M-%S', time.localtime())
         name = self.config.simulation_name.replace('$(id)', simulation_id)
         sim_data_collector = SimulationDataCollector(self.vehicle, beamng, brewer.decal_road, brewer.params,
-                                                     vehicle_state_reader=vehicle_state_reader,
+                                                     vehicle_state_reader=self.vehicle_state_reader,
                                                      camera=self.camera,
                                                      simulation_name=name)
 
         sim_data_collector.get_simulation_data().start()
         try:
             brewer.bring_up()
+            # Make the simulation run for 1 step to fill the car camera buffer
+            beamng.step(1)
+
             from keras.models import load_model
             if not self.model:
                 self.model = load_model(self.model_file)
             predict = NvidiaPrediction(self.model, self.config)
+
             iterations_count = 1000
             idx = 0
             while True:
@@ -101,12 +107,18 @@ class BeamNGNvidiaOob(BeamNGEvaluator):
 
                 sim_data_collector.collect_current_data(oob_bb=False)
                 last_state: SimulationDataRecord = sim_data_collector.states[-1]
-                if points_distance(last_state.pos, waypoint_goal.position) < 6.0:
+
+                dist = points_distance(last_state.pos, waypoint_goal.position)
+                if dist < 6.0:
                     break
 
                 if last_state.is_oob:
                     break
-                img = vehicle_state_reader.sensors['cam_center']['colour'].convert('RGB')
+
+                img = Image.fromarray(brewer.car_cameras.cam_center
+                                      .stream_colour(320 * 160 * 4).reshape(160, 320, 4)).convert('RGB')
+                # img.save(f"../img/{datetime.now().strftime('%d-%m_%H-%M-%S-%f')[:-3]}.jpg")
+
                 steering_angle, throttle = predict.predict(img, last_state)
                 self.vehicle.control(throttle=throttle, steering=steering_angle, brake=0)
                 beamng.step(steps)
@@ -149,12 +161,13 @@ class BeamNGNvidiaOob(BeamNGEvaluator):
 
 
 if __name__ == '__main__':
-    config = BeamNGConfig()
-    inst = BeamNGNvidiaOob(config)
-    while True:
-        seed_storage = SeedStorage('basic5')
-        for i in range(1, 11):
-            member = BeamNGMember.from_dict(seed_storage.load_json_by_index(i))
-            member.clear_evaluation()
-            inst.evaluate([member])
-            log.info(member)
+    cfg = BeamNGConfig()
+    inst = BeamNGNvidiaOob(cfg)
+
+    seed_storage = SeedStorage('basic5')
+    for i in range(1, 11):
+        mbr = BeamNGMember.from_dict(seed_storage.load_json_by_index(i))
+        mbr.clear_evaluation()
+        inst.evaluate([mbr])
+        log.info(mbr)
+
