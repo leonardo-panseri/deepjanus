bbb379a1c7fdcb12360c53aefaa609bbdf8cbfa2
Leonardo Panseri <leonardo.panseri@gmail.com>
2024-02-08 16:57:15 +0100

    Cleanup repository

--- DeepJanus-UE/test.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-try:
-    print("server running")
-    subprocess.run(["java", "-jar", "Sikuli-jars//sikulixapi-2.0.4.jar", "-s"])
-    print("Done")
-except subprocess.CalledProcessError as e:
-    print(e)
--- index.md
+++
@@ -1,1 +0,0 @@
-[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.3878037.svg)](https://doi.org/10.5281/zenodo.3878037)
