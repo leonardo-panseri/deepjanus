f4eb2e96dc4cb60b95a130c788781b553531be9e
Leonardo Panseri <leonardo.panseri@gmail.com>
2024-03-22 12:20:15 +0100

    Modify experiments folder structure

--- experiments/collect_data.py
+++ experiments/collect_data.py
@@ -4,11 +4,18 @@ from pathlib import Path
 def move_all_last_archives(system):
     experiments_path = Path(f'../DeepJanus-{system}/data/experiments')
     for experiment in experiments_path.glob('*/'):
+        if not experiment.is_dir():
+            continue
         last_archive = experiment.glob('gen99/archive/*.json')
-        new_path = Path(f'./{system}/{experiment.name}/archive').absolute()
+        logs = experiment.glob('*.log')
+        exp_name = experiment.name.split('_')[0] + '_unsafe'
+        exp_idx = experiment.name.split('_')[1]
+        new_path = Path(f'./{system}/{exp_name}/{exp_idx}').absolute()
         new_path.mkdir(parents=True, exist_ok=True)
+        for log in logs:
+            log.rename(new_path.joinpath(log.name))
         for ind in last_archive:
-            ind.rename(new_path.joinpath(ind.name))
+            ind.rename(new_path.joinpath('archive', ind.name))
 
 
 if __name__ == '__main__':
